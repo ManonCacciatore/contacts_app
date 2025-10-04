1b37b623e84c2eb596daf317aec3b4aaa0eea7dd
“ManonCacciatore” <cacciatoremanon8@gmail.com>
2025-10-04 20:21:18 +0200

    Méthode update

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,4 +1,5 @@
 import getTemplate from "../contact/template";
+import DB from "../../DB";
 
 export default class Contact {
   constructor(data) {
@@ -18,24 +19,51 @@ export default class Contact {
   el.append(this.domElt);
 
   this.initEvents();
+  }
+
+  update(field, value) {
+    this[field] = value;
+
+    if (field === "firstname") {
+      this.domElt.querySelector(".input-firstname").previousElementSibling.innerText = value;
+    }
+    else if (field === "lastname") {
+      this.domElt.querySelector(".input-lastname").previousElementSibling.innerText = value;
+    }
+    else if (field === "email") {
+      this.domElt.querySelector(".input-email").previousElementSibling.innerText = value;
+    }
+  }
+
+  async save() {
+    await DB.updateOneById(this.id, {
+      firstname: this.firstname,
+      lastname: this.lastname,
+      email: this.email
+    });
+  }
+
+  initEvents() {
+    // DELETE
+    this.domElt.querySelector(".btn-delete").addEventListener("click", () => {
+        window.ContactList.deleteOneById(this.id);
+        this.domElt.remove();
+    });
+
+    // EDIT
+    this.domElt.querySelector(".btn-edit").addEventListener("click", () => {
+        this.domElt.classList.add("isEditing");
+    });
+
+    // CHECK
+    this.domElt.querySelector(".btn-check").addEventListener("click", async () => {
+      this.update("firstname", this.domElt.querySelector(".input-firstname").value);
+      this.update("lastname", this.domElt.querySelector(".input-lastname").value);
+      this.update("email", this.domElt.querySelector(".input-email").value);
+
+      await this.save();
+
+      this.domElt.classList.remove("isEditing");
+    });
+  }
 }
-
-initEvents() {
-  
-  this.domElt.querySelector(".btn-edit")
-    .addEventListener("click", () => {
-      this.domElt.classList.toggle("isEditing");
-    });
-
-  this.domElt.querySelector(".btn-delete")
-    .addEventListener("click", (e) => {
-      window.ContactList.deleteOneById(this.id);
-      this.domElt.remove()
-
-    });
-
-  this.domElt.querySelector(".btn-check")
-    .addEventListener("click", () => {
-      alert("Validation !");
-    });
-}}
